c25f9903fd878f8a422e75c6bbaefde3312ab861
AnyMal <lksdro01@gmail.com>
2023-02-11 23:02:48 -0300

    Agregar pagina basica sobre detalles de productos

---
+++ pages/products/[id].js
@@ -0,0 +1,9 @@
+import PageLayout from '../../components/PageLayout'
+
+export default function ProductPage({ product }) {
+  return (
+    <PageLayout title='Producto x'>
+        <h1>Detail Product Page</h1>
+    </PageLayout>
+  )
+}
