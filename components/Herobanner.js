6fec6d0562531154f36e875644d3d40edeff880e
AnyMal <lksdro01@gmail.com>
2023-02-13 20:28:18 -0300

    Agregar estilos basicos a herobanner

--- components/Herobanner.js
+++ components/Herobanner.js
@@ -1,15 +1,48 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Herobanner = () => {
+import { useNextSanityImage } from 'next-sanity-image'
+
+import { Container, Button } from '@mui/material'
+
+import { urlFor, client } from '../lib/client'
+import { Box } from '@mui/system'
+
+
+const Herobanner = ({ heroBanner } ) => {
+
+  const imageProps = useNextSanityImage(
+		client,
+		heroBanner.imagen
+	);
+
   return (
-    <div className='herobanner-container'>
-        <img src='' alt='imagen' />
-        <div className='herobanner-content'>
-            <h1>Titulo</h1>
-            <p>Descripcion</p>
-            <button>Boton</button>
-        </div>
-    </div>
+    <Box sx={{
+      position: 'relative',
+      }}>
+      <Box sx={{
+        
+        }}>
+        <img src={urlFor(heroBanner.imagen)} alt='imagen banner' width={'100%'} height={500}/>
+      </Box>
+        
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          position: 'absolute',
+          bottom: '20%',
+          left: '15%',
+          color: 'var(--dark-color)',
+        }}>
+            <h1>{ heroBanner.titulo } </h1>
+            <p>{ heroBanner.descripcion } </p>
+            <p>Hasta un {heroBanner.descuento}% de descuento</p>
+            <Button variant='contained' sx={{
+              backgroundColor: 'var(--alert-color)',
+              color: 'var(--light-color)',
+            }}>Ver ofertas</Button>
+        </Box>
+    </Box>
   )
 }
 
